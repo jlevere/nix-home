09e7030389fab6954c35e12f903cc0c02f378ada
jLevere <71566629+jLevere@users.noreply.github.com>
2024-12-04 13:02:13 -0500

    formated files manually

--- dotfiles/default.nix
+++ dotfiles/default.nix
@@ -18,7 +18,6 @@
     open-sans
   ];
 
-
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
@@ -31,14 +30,12 @@
     generateCaches = true;
   };
 
-
   home.file."${config.home.homeDirectory}/.ssh/allowed_signers".text = let
     pubkey = builtins.readFile (builtins.fetchurl {
       url = "https://github.com/jLevere.keys";
       sha256 = "1g87mxaaizyn8y5l0mdkxh14gywp4xjxlhxx2327m1q528bzn7gp";
     });
-  in
-    "71566629+jLevere@users.noreply.github.com ${pubkey}";
+  in "71566629+jLevere@users.noreply.github.com ${pubkey}";
 
   programs.git = {
     enable = true;
@@ -49,12 +46,10 @@
     userName = "jLevere";
 
     extraConfig = {
-
       commit.gpgsign = true;
       gpg.format = "ssh";
       gpg.ssh.allowedSignersFile = "${config.home.homeDirectory}/.ssh/allowed_signers";
       user.signingkey = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
-
     };
   };
 
@@ -75,11 +70,11 @@
       }
     ];
     functions = {
-        __fish_command_not_found_handler = {
-          body = "__fish_default_command_not_found_handler $argv[1]";
-          onEvent = "fish_command_not_found";
-        };
+      __fish_command_not_found_handler = {
+        body = "__fish_default_command_not_found_handler $argv[1]";
+        onEvent = "fish_command_not_found";
       };
+    };
   };
 
   home.activation.configure-tide = lib.hm.dag.entryAfter ["writeBoundary"] ''
--- flake.nix
+++ flake.nix
@@ -23,8 +23,6 @@
       url = "github:kamadorueda/alejandra/3.1.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-
   };
 
   outputs = {

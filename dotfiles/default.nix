920a39535b089d34129494c2c70aa5f1b337f975
jLevere <71566629+jLevere@users.noreply.github.com>
2024-12-14 08:21:46 +0100

    updated fish prompt

--- dotfiles/default.nix
+++ dotfiles/default.nix
@@ -78,7 +78,7 @@
   };
 
   home.activation.configure-tide = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    ${pkgs.fish}/bin/fish -c "tide configure --auto --style=Lean --prompt_colors='True color' --show_time=No --lean_prompt_height='Two lines' --prompt_connection=Solid --prompt_connection_andor_frame_color=Dark --prompt_spacing=Sparse --icons='Many icons' --transient=No"
+    ${pkgs.fish}/bin/fish -c "tide configure --auto --style=Lean --prompt_colors='True color' --show_time=No --lean_prompt_height='Two lines' --prompt_connection=Disconnected --prompt_connection_andor_frame_color=Dark --prompt_spacing=Compact --icons='Many icons' --transient=No"
   '';
 
   programs.zellij = {
